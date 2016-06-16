db7ba8bc713305986fc94dae94ab72bd5dcf3356
andreztz <andreztz@gmail.com>
2016-06-16 20:49:51 -0300

    setup database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
 from . import config
 from flask import Flask
 from flask import render_template
+import os
+
+def setup_database():
+    if not os.path.isfile('ztzinfo.db'):
+        from .dbconn import session
+        from .models import Base, User
+
+        Base.metadata.create_all()
+        user = User(username=os.environ['MAIL_USERNAME'])
+        user.hash_password(os.environ['MAIL_PASSWORD'])
+        session.add(user)
+        session.commit()
 
 
 app = Flask(__name__, static_url_path='')
 
 app.config.from_object(config.DevelopmentConfig)
+
+setup_database()
 
 from .blog import blog
 from .admin import admin
