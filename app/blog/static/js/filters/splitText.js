23b71f544952daeced73da7d9d4a6298679c09ca
andreztz <andreztz@gmail.com>
2016-06-18 18:57:09 -0300

    add .idea gitignore

--- app/blog/static/js/filters/splitText.js
+++ app/blog/static/js/filters/splitText.js
@@ -1,7 +1,6 @@
 angular.module('Blog').filter('splitText', function () {
     return function (input, end, sep) {
         sep = typeof sep !== 'undefined' ? sep : '';
-        console.log(input, end, sep)
         return input.substring(0, end) + sep;
     };
 });
